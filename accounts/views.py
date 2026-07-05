a528254b088e86d91a11000246c5aecef660230e
rad129ratul <ratulrs29@gmail.com>
2026-07-05 18:43:13 +0600

    add protected test endpoint for auth verification

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def ping(request):
+    return Response({
+        "message": "pong",
+        "user": request.user.username,
+    })
