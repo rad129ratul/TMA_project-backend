73b5bf6ece2ecbadbcabbc22b2cdfa93a19ebd09
rad129ratul <ratulrs29@gmail.com>
2026-07-06 19:27:16 +0600

    add task list/create endpoint with date filter

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Task.objects.filter(owner=self.request.user)
+
+        date_param = self.request.query_params.get("date")
+        if date_param:
+            queryset = queryset.filter(due_date=date_param)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
